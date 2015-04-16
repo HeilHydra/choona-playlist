3d758f2eb3992dc554541a26f1421675481439e4
Oliver Woodings <oliver.woodings@qubitdigital.com>
2015-04-16 14:30:23 +0100

    More progress

--- service.js
+++ service.js
@@ -2,10 +2,6 @@ var Waterway = require("waterway");
 var config = require("config");
 var PlaylistManager = require("./lib/PlaylistManager");
 
-var NRP = require("node-redis-pubsub");
-var lame = require("lame");
-var Speaker = require("speaker");
-
 var service = new Waterway(config.waterway);
 var playlistManager = new PlaylistManager(service);
 
@@ -37,38 +33,4 @@ service.request("playlist", ":playlistId", "getStatus")
 service.stream("playlist", ":playlistId", "stream")
   .writable(function (stream, req) {
     playlistManager.get(req.params.playlistId).streamTo(stream);
-  });
-
-var nrp = new NRP();
-nrp.on("*", function (data, key) {
-  key = key.split(":").map(function (part) {
-    if (part !== "*") {
-      return new Buffer(part, "base64").toString("utf8");
-    }
-    return "*";
-  });
-  // console.log(key.join(":"), data);
-});
-
-var client = new Waterway();
-client
-  .request("playlist", "1", "add", "5ipZFjdQSogmpRqUaKTCLW")
-  .send()
-  .then(function () {
-    return client.request("playlist", "1", "add", "0KQZquKoSYtflDvj71SlGj").send();
-  })
-  .then(function () {
-    return client.request("playlist", "1", "getQueue").send();
-  })
-  .then(function (queue) {
-    console.log(queue.data);
-    return client.stream("playlist", "1", "stream").readable();
-  })
-  .then(function (stream) {
-    stream
-      .pipe(new lame.Decoder())
-      .pipe(new Speaker());
-  })
-  .catch(function (err) {
-    console.log(err);
   });
